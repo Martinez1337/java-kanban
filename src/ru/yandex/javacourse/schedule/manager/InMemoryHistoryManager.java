9afe2ec7b69323917f8ebfa59f50d7d1e31ee01b
Martinez1337 <90857594+Martinez1337@users.noreply.github.com>
2025-10-08 21:06:30 +0300

    Implement double linked list with hashmap indexation

--- src/ru/yandex/javacourse/schedule/manager/InMemoryHistoryManager.java
+++ src/ru/yandex/javacourse/schedule/manager/InMemoryHistoryManager.java
@@ -1,10 +1,9 @@
 package ru.yandex.javacourse.schedule.manager;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 import ru.yandex.javacourse.schedule.tasks.Task;
+import ru.yandex.javacourse.schedule.util.entities.Node;
 
 /**
  * In memory history manager.
@@ -12,24 +11,88 @@ import ru.yandex.javacourse.schedule.tasks.Task;
  * @author Vladimir Ivanov (ivanov.vladimir.l@gmail.com)
  */
 public class InMemoryHistoryManager implements HistoryManager {
-	private final LinkedList<Task> history = new LinkedList<>();
+	private final Map<Integer, Node<Task>> taskMap = new HashMap<>();
 
-	public static final int MAX_SIZE = 10;
+	private Node<Task> head;
+	private Node<Task> tail;
+
+	private void linkLast(Task task) {
+		if (task == null) {
+			return;
+		}
+
+		remove(task.getId()); // Удаляем существующую задачу с таким же id, если такая существует
+
+		Task taskCopy = new Task(task); // Создаем копию задачи чтобы избежать изменений извне
+		final Node<Task> oldTail = tail;
+		final Node<Task> newNode = new Node<>(taskCopy, oldTail, null);
+		tail = newNode;
+
+		if (oldTail == null) {
+			head = newNode;
+		} else {
+			oldTail.next = newNode;
+		}
+
+		taskMap.put(task.getId(), newNode); // Добавляем в map для быстрого доступа
+	}
+
+	private void removeNode(Node<Task> node) {
+		if (node == null) {
+			return;
+		}
+
+		final Node<Task> next = node.next;
+		final Node<Task> prev = node.prev;
+
+		// Удаляем узел из списка
+		if (prev == null) {
+			head = next;
+		} else {
+			prev.next = next;
+			node.prev = null;
+		}
+
+		if (next == null) {
+			tail = prev;
+		} else {
+			next.prev = prev;
+			node.next = null;
+		}
+
+		taskMap.remove(node.data.getId()); // Удаляем указатель из taskMap
+	}
+
+	/**
+	 * Собирает все задачи из списка в ArrayList в порядке от новой к старой
+	 */
+	private List<Task> getTasks() {
+		List<Task> tasks = new ArrayList<>();
+		Node<Task> current = head;
+
+		while (current != null) {
+			tasks.add(current.data);
+			current = current.next;
+		}
+
+		return tasks;
+	}
 
 	@Override
 	public List<Task> getHistory() {
-		return new ArrayList<>(history);
+		return getTasks();
 	}
 
 	@Override
 	public void addTask(Task task) {
-		if (task == null) {
-			return;
-		}
-		history.add(task);
-		if (history.size() > MAX_SIZE) {
-			history.removeFirst();
-		}
+		linkLast(task);
+	}
 
+	@Override
+	public void remove(int id) {
+		Node<Task> node = taskMap.get(id);
+		if (node != null) {
+			removeNode(node);
+		}
 	}
 }
