1011ff2999c4b72f83245712d1b048a438231474
Martinez1337 <90857594+Martinez1337@users.noreply.github.com>
2025-12-15 23:05:27 +0300

    Add duration and startTime field in tasks

--- src/ru/yandex/javacourse/schedule/tasks/Subtask.java
+++ src/ru/yandex/javacourse/schedule/tasks/Subtask.java
@@ -1,43 +1,54 @@
 package ru.yandex.javacourse.schedule.tasks;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Subtask extends Task {
-	protected int epicId;
+    protected int epicId;
 
-	public Subtask(int id, String name, String description, TaskStatus status, int epicId) {
-		super(id, name, description, status);
-		if (id == epicId) {
-			throw new IllegalArgumentException("Subtask id must not be equal to epic id");
-		}
-		this.epicId = epicId;
-	}
+    public Subtask(
+			int id, String name, String description, TaskStatus status,
+			Duration duration, LocalDateTime startTime, int epicId
+	) {
+        super(id, name, description, status, duration, startTime);
+        if (id == epicId) {
+            throw new IllegalArgumentException("Subtask id must not be equal to epic id");
+        }
+        this.epicId = epicId;
+    }
 
-	public Subtask(String name, String description, TaskStatus status, int epicId) {
-		super(name, description, status);
-		this.epicId = epicId;
-	}
+    public Subtask(
+			String name, String description, TaskStatus status,
+			Duration duration, LocalDateTime startTime, int epicId
+	) {
+        super(name, description, status, duration, startTime);
+        this.epicId = epicId;
+    }
 
-	public Subtask(Subtask other) {
-		super(other);
-		this.epicId = other.epicId;
-	}
+    public Subtask(Subtask other) {
+        super(other);
+        this.epicId = other.epicId;
+    }
 
-	public int getEpicId() {
-		return epicId;
-	}
+    public int getEpicId() {
+        return epicId;
+    }
 
-	@Override
-	public TaskType getType() {
-		return TaskType.SUBTASK;
-	}
+    @Override
+    public TaskType getType() {
+        return TaskType.SUBTASK;
+    }
 
-	@Override
-	public String toString() {
-		return "Subtask{" +
-				"id=" + id +
-				", epicId=" + epicId +
-				", name='" + name + '\'' +
-				", status=" + status +
-				", description='" + description + '\'' +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "id=" + id +
+                ", epicId=" + epicId +
+                ", name='" + name + '\'' +
+                ", status=" + status +
+                ", description='" + description + '\'' +
+                ", duration='" + duration + '\'' +
+                ", startTime='" + startTime + '\'' +
+                '}';
+    }
 }
