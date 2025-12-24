9930a5f6f5cead0a17d12d7fa4134f5531995c52
Martinez1337 <90857594+Martinez1337@users.noreply.github.com>
2025-12-25 00:46:22 +0300

    Fix Main

--- src/ru/yandex/javacourse/schedule/Main.java
+++ src/ru/yandex/javacourse/schedule/Main.java
@@ -18,20 +18,20 @@ public class Main {
 		// Создание
 		Task task1 = new Task("Task #1", "Task1 description", NEW, null, null);
 		Task task2 = new Task("Task #2", "Task2 description", IN_PROGRESS, null, null);
-		final int taskId1 = manager.addNewTask(task1);
-		final int taskId2 = manager.addNewTask(task2);
+		final int taskId1 = manager.createTask(task1);
+		final int taskId2 = manager.createTask(task2);
 
 		Epic epic1 = new Epic("Epic #1", "Epic1 description");
 		Epic epic2 = new Epic("Epic #2", "Epic2 description");
-		final int epicId1 = manager.addNewEpic(epic1);
-		final int epicId2 = manager.addNewEpic(epic2);
+		final int epicId1 = manager.createEpic(epic1);
+		final int epicId2 = manager.createEpic(epic2);
 
 		Subtask subtask1 = new Subtask("Subtask #1-1", "Subtask1 description", NEW, null, null, epicId1);
 		Subtask subtask2 = new Subtask("Subtask #2-1", "Subtask1 description", NEW, null, null, epicId1);
 		Subtask subtask3 = new Subtask("Subtask #3-2", "Subtask1 description", DONE, null, null, epicId2);
-		manager.addNewSubtask(subtask1);
-		final Integer subtaskId2 = manager.addNewSubtask(subtask2);
-		final Integer subtaskId3 = manager.addNewSubtask(subtask3);
+		manager.createSubtask(subtask1);
+		final Integer subtaskId2 = manager.createSubtask(subtask2);
+		final Integer subtaskId3 = manager.createSubtask(subtask3);
 
 		printAllTasks(manager);
 
